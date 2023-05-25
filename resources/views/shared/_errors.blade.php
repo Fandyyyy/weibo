6265acbf3a7e696eab47a30276df8e2abb0175e9
Fandyyyy <2425706870@qq.com>
2023-05-25 17:45:40 +0000

    中文的错误提示信息

--- resources/views/shared/_errors.blade.php
+++ resources/views/shared/_errors.blade.php
@@ -1,8 +1,8 @@
-@if(count($errors) > 0)
+@if (count($errors) > 0)
   <div class="alert alert-danger">
     <ul>
       @foreach($errors->all() as $error)
-        <li>{{$error}}</li>
+        <li>{{ $error }}</li>
       @endforeach
     </ul>
   </div>
