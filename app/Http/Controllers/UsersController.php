7a0e4e4025bff60d041caec8bc977295e88e5435
Fandyyyy <2425706870@qq.com>
2023-05-25 17:48:53 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,18 +19,19 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request,[
-           'name'=>'required|unique:users|max:50',
-           'email'=>'required|email|unique:users|max:255',
-           'password'=>'required|confirmed|min:6'
-        ]);
-        $user = User::create([
-            'name'=>$request->name,
-            'email'=>$request->email,
-            'password'=>bcrypt($request->password),
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
         ]);
 
-        session()->flash('success','欢迎，你将在这里开启一段新的旅程~');
-        return redirect()->route('users.show',[$user]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 }
