544f5ffdeb559100392ea707ba74b4d9fe621ace
Fandyyyy <2425706870@qq.com>
2023-05-28 08:04:30 +0000

    重置密码

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,6 @@ Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('conf
 Route::get('password/reset', 'PasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email', 'PasswordController@sendResetLinkEmail')->name('password.email');
 
-Route::get('password/reset/{token}','PasswordController@showRequestForm')->name('password.reset');
+Route::get('password/reset/{token}','PasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset','PasswordController@reset')->name('password.update');
 
